028e04494d7fd30677fc9000f52f62a50320dd65
Hengwei Yang <101932649+viyur@users.noreply.github.com>
2026-04-18 18:09:55 -0400

    Adjust timeout and sliding window size parameters
    Reduced TIMEOUT_SEC and SLIDING_WINDOW_SIZE for performance in config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ SRFT Configuration
 DEFAULT_SERVER_PORT = 5000
 DEFAULT_CLIENT_PORT = 5001
 CHUNK_SIZE = 1024   # Bytes per DATA packet payload
-TIMEOUT_SEC = 0.5   # Retransmission timeout
+TIMEOUT_SEC = 0.03   # Retransmission timeout
 FILES_DIR = "files"
 REPORT_FILE = "transfer_report.txt"
 
-SLIDING_WINDOW_SIZE = 64   # 小于包数，可以测试window滑动
+SLIDING_WINDOW_SIZE = 16   # 小于包数，可以测试window滑动
 ACK_EVERY_N = 5     # client每收到5个包，就给server发一个ACK
 ACK_INTERVAL_SEC = 0.02
 RECV_BUFFER_SIZE = 65535
